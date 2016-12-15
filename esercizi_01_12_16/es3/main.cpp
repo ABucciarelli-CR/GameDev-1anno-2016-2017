6555c08a08d9ae3f7ed668279ecaffdda3213cd8
alessio bucciarelli <alessiobuccia@gmail.com>
2016-12-15 09:40:01 +0100

    finiti esercizi 1, 2 e 3

--- esercizi_01_12_16/es3/main.cpp
+++ esercizi_01_12_16/es3/main.cpp
@@ -3,40 +3,67 @@
 #include <time.h>
 #include <cstdlib>
 #define MAX 5
-#define Ntrappole 3
+#define Nbloks 3
+#define Nmonsters 3
 
 using namespace std;
 
 int dungeon[MAX][MAX];
+int dungeonapp[MAX][MAX];
 int X,Xprim,Y,Yprim;
 
 void dungeon_creation();
 void matrix_stamp();
-void position_trap();
+void position_monsters();
 void position_treasure();
 void position_player();
-void player_move();
+void position_unbrecable_bloks();
 
+int player_move();
+int monsters_move(int Jmonster, int Imonster);
 int win(int WinImput);
-int dungeon_update();
+int dungeon_update(int result);
 
 
 int main()
 {
     int retry=1;
-    int WinLoseNull;
+    int monsters_moved=0;
+    int WinLoseNull=0;
+    int i,j;
 
     do
     {
         dungeon_creation();
-        position_trap();
+        position_monsters();
         position_treasure();
         position_player();
+        position_unbrecable_bloks();
         matrix_stamp();
         do
         {
-            player_move();
-            WinLoseNull=dungeon_update();
+            WinLoseNull=player_move();
+            WinLoseNull=dungeon_update(WinLoseNull);
+            if(WinLoseNull!=2 && WinLoseNull!=1)
+            {
+               do
+                {
+                    for(j=0;j<MAX;j++)
+                    {
+                        for(i=0;i<MAX;i++)
+                        {
+                            if(dungeon[j][i]==5)
+                            {
+                                WinLoseNull=monsters_move(j, i);
+                                monsters_moved++;
+                            }
+                        }
+                    }
+                }
+                while(monsters_moved<Nmonsters);
+            }
+            matrix_stamp();
+            dungeon_update(WinLoseNull);
             matrix_stamp();
             retry=win(WinLoseNull);
         }
@@ -76,11 +103,11 @@ void dungeon_creation()
 }
 
 /////posizionamento trappole
-void position_trap()
+void position_monsters()
 {
     int i,j;
     srand(time(NULL));
-    for(int k=0; k<Ntrappole;)
+    for(int k=0; k<Nmonsters;)
     {
         i=rand()%MAX;
         j=rand()%MAX;
@@ -130,11 +157,30 @@ void position_player()
     while(dungeon[j][i]!=7);
 }
 
-/////movimento del giocatore
-void player_move()
+//////posizionamento blocchi indistruttibili
+void position_unbrecable_bloks()
 {
+    int i,j;
+    int bloks=0;
+    srand(time(NULL));
+    do
+    {
+        i=rand()%MAX;
+        j=rand()%MAX;
+        if(dungeon[j][i]==0)
+        {
+            dungeon[j][i]=6;
+            bloks++;
+        }
+    }
+    while(bloks<Nbloks);
+}
 
+/////movimento del giocatore
+int player_move()
+{
     char moves;
+    int result=0;
     int quit=0;
     int Wok=0;
     int Aok=0;
@@ -142,22 +188,22 @@ void player_move()
     int Dok=0;
 ////////controllo possibili movimenti
     cout << "Fai la tua mossa:" << endl;
-    if(Y>0)
+    if(Y>0 && dungeon[Y-1][X]!=6)
     {
         cout << "W => su" << endl;
         Wok=1;
     }
-    if(X>0)
+    if(X>0 && dungeon[Y][X-1]!=6)
     {
         cout << "A => sx" << endl;
         Aok=1;
     }
-    if(Y<MAX-1)
+    if(Y<MAX-1 && dungeon[Y+1][X]!=6)
     {
         cout << "S => giu" << endl;
         Sok=1;
     }
-    if(X<MAX-1)
+    if(X<MAX-1 && dungeon[Y][X+1]!=6)
     {
         cout << "D => dx" << endl;
         Dok=1;
@@ -229,13 +275,19 @@ void player_move()
     }
     while(quit==0);
 
+    if(dungeon[Yprim][Xprim]==5)
+    {
+        result=2;
+    }
+
+    return result;
 }
 
 /////aggiornamento dungeon
-int dungeon_update()
+int dungeon_update(int result)
 {
 ////risultati: 1 vinto, 2 Perso, 0 Nulla
-    int result;
+    int j,i;
 
     if(dungeon[Y][X]==5)
     {
@@ -245,10 +297,22 @@ int dungeon_update()
     {
         result=1;
     }
-    else
+    else if(result!=2)
     {
         dungeon[Y][X]=7;
         dungeon[Yprim][Xprim]=0;
+    }
+
+    for(j=0;j<MAX;j++)
+    {
+        for(i=0;i<MAX;i++)
+        {
+            if(dungeonapp[j][i]==5)
+            {
+                dungeonapp[j][i]=0;
+                dungeon[j][i]=5;
+            }
+        }
     }
 
     return result;
@@ -278,6 +342,84 @@ int win(int WinImput)
         }
     }
     while(result!=0 && result!=1);
+
+    return result;
+}
+
+/////movimento mostri
+int monsters_move(int Jmonster, int Imonster)
+{
+    int result=0;
+    int random_move;
+    int move_executed=0;
+    srand(time(NULL));
+
+    do
+    {
+        random_move=rand()%4;
+        switch(random_move)
+        {
+            case 0:
+                if((dungeon[Jmonster+1][Imonster]==0 || dungeon[Jmonster+1][Imonster]==7) && Jmonster+1<MAX)
+                {
+                    if(dungeon[Jmonster+1][Imonster]==7)
+                    {
+                        result=2;
+                    }
+
+                    dungeonapp[Jmonster+1][Imonster]=5;
+                    dungeon[Jmonster][Imonster]=0;
+                    move_executed=1;
+                }
+                break;
+
+            case 1:
+                if((dungeon[Jmonster-1][Imonster]==0 || dungeon[Jmonster-1][Imonster]==7) && Jmonster-1>=0)
+                {
+                    if(dungeon[Jmonster-1][Imonster]==7)
+                    {
+                        result=2;
+                    }
+
+                    dungeonapp[Jmonster-1][Imonster]=5;
+                    dungeon[Jmonster][Imonster]=0;
+                    move_executed=1;
+                }
+                break;
+
+            case 2:
+                if((dungeon[Jmonster][Imonster+1]==0 || dungeon[Jmonster][Imonster+1]==7) && Imonster+1<MAX)
+                {
+                    if(dungeon[Jmonster][Imonster+1]==7)
+                    {
+                        result=2;
+                    }
+
+                    dungeonapp[Jmonster][Imonster+1]=5;
+                    dungeon[Jmonster][Imonster]=0;
+                    move_executed=1;
+                }
+                break;
+
+            case 3:
+                if((dungeon[Jmonster][Imonster-1]==0 || dungeon[Jmonster][Imonster-1]==7) && Imonster-1>=0)
+                {
+                    if(dungeon[Jmonster][Imonster-1]==7)
+                    {
+                        result=2;
+                    }
+
+                    dungeonapp[Jmonster][Imonster-1]=5;
+                    dungeon[Jmonster][Imonster]=0;
+                    move_executed=1;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+    while(move_executed==0);
 
     return result;
 }
