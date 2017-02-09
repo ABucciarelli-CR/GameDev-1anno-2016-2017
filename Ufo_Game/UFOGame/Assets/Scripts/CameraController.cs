be2811e5f7c802ef6db1fb99d3a3c99cea20d2af
alessio bucciarelli <alessiobuccia@gmail.com>
2017-02-09 12:52:54 +0100

    commmit progetto Ufo

---
+++ Ufo_Game/UFOGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform playerTransform;
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_offset = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = playerTransform.position + _offset;
+	}
+}
