bb2a208ba3968a6e992ab37f02f36b003053e89b
ABucciarelli-CR <alessiobuccia@gmail.com>
2017-04-02 18:30:03 +0200

    aggiunta della granata

---
+++ Survaivalsciuter+player_movement+granata/Assets/Scripts/Player/GranadeXplosion.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GranadeXplosion : MonoBehaviour 
+{
+	public float WaitToXplode=2.0f;
+	public int Damage = 500;
+	public float DamageArea = 8.0f;
+	public float LaunchSpeed = 700;
+	public Rigidbody _rb;
+
+	RaycastHit shootHit;
+
+	void Awake()
+	{
+		_rb.AddForce (transform.forward * LaunchSpeed);
+	}
+
+	void Update () 
+	{
+		WaitToXplode-=Time.deltaTime;
+		if (WaitToXplode <= 0.0f) 
+		{
+			xplode ();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		
+	}
+
+	private void xplode ()
+	{
+		Collider[] inRange = Physics.OverlapSphere (transform.position, DamageArea);
+		foreach (Collider col in inRange) 
+		{
+			EnemyHealth enemyHealth = col.GetComponent<EnemyHealth> ();
+			if(enemyHealth != null)
+			{
+				enemyHealth.TakeGranadeDamage(Damage);
+			}
+		}
+
+		Destroy (gameObject);
+	}
+}
---
+++ Survaivalsciuter+player_movement+granata/Assets/Scripts/Player/PlayerGranadeLaunch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGranadeLaunch : MonoBehaviour 
+{
+	public int MaxGranade = 3;
+	public float timeBetweenGranades = 2.0f;
+	public GameObject Granade;
+
+	void Update () 
+	{
+		
+		if(Input.GetMouseButtonDown(1) && MaxGranade > 0)
+		{
+			MaxGranade -= 1;
+			Instantiate (Granade, transform.position, transform.rotation);
+		}
+	}
+}
